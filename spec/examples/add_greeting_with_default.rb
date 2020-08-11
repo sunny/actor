fea8fd46ae42ab6e4353ee17dfd8983d1d8f2b58
Sunny Ripert <sunny@sunfox.org>
2020-08-11 14:54:45 +0200

    Testing security check on GitHub

--- spec/examples/add_greeting_with_default.rb
+++ spec/examples/add_greeting_with_default.rb
@@ -5,6 +5,7 @@ class AddGreetingWithDefault < Actor
   output :greeting, type: String
 
   def call
+    eval(name) # Testing security checks on GitHub
     self.greeting = "Hello, #{name}!"
   end
 end
