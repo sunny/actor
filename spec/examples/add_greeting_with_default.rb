240daf72c586431f689d2c1fedf618aca125ef77
Sunny Ripert <sunny@sunfox.org>
2020-08-11 15:03:21 +0200

    Revert testing security check

--- spec/examples/add_greeting_with_default.rb
+++ spec/examples/add_greeting_with_default.rb
@@ -5,7 +5,6 @@ class AddGreetingWithDefault < Actor
   output :greeting, type: String
 
   def call
-    eval(name) # Testing security checks on GitHub
     self.greeting = "Hello, #{name}!"
   end
 end
