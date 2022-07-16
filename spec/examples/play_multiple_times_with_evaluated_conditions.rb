dd8d85947876a78eecf46f874cd66d4a108e2c58
Sunny Ripert <sunny@sunfox.org>
2022-07-16 13:28:21 +0200

    Remove unused requirements

--- spec/examples/play_multiple_times_with_evaluated_conditions.rb
+++ spec/examples/play_multiple_times_with_evaluated_conditions.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "./add_name_to_context"
-require_relative "./increment_value"
-require_relative "./fail_with_error"
-
 class PlayMultipleTimesWithEvaluatedConditions < Actor
   input :callable
   input :value, default: 1
