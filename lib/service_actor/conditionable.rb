687a41dca8ed538b79aa2dbfd8b0db2740329c63
Anton <profox.rus@gmail.com>
2022-09-04 19:17:57 +0300

    Added advanced mode

--- lib/service_actor/conditionable.rb
+++ lib/service_actor/conditionable.rb
@@ -13,31 +13,46 @@
 #             exist: -> provider { PROVIDERS.include?(provider) },
 #           }
 #   end
+#
+#   class Pay < Actor
+#     input :provider,
+#           must: {
+#             exist: {
+#               state: -> provider { PROVIDERS.include?(provider) },
+#               message: (lambda do |_input_key, _must_key, value|
+#                 "The specified provider \"#{value}\" was not found."
+#               end)
+#             }
+#           }
+#   end
 module ServiceActor::Conditionable
   def self.included(base)
     base.prepend(PrependedMethods)
   end
 
   module PrependedMethods
-    def _call
+    def _call # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       self.class.inputs.each do |key, options|
         next unless options[:must]
 
-        options[:must].each do |name, check|
+        options[:must].each do |name, content|
           value = result[key]
+
+          if content.is_a?(Hash)
+            check, message = content.values_at(:state, :message)
+            error_text = message.call(key, name, value)
+          else
+            check = content
+            error_text = "Input #{key} must #{name} but was #{value.inspect}"
+          end
+
           next if check.call(value)
 
-          raise ServiceActor::ArgumentError, error_text_with(key, name, value)
+          raise ServiceActor::ArgumentError, error_text
         end
       end
 
       super
-    end
-
-    private
-
-    def error_text_with(key, name, value)
-      "Input #{key} must #{name} but was #{value.inspect}"
     end
   end
 end
