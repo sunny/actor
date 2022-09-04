50a568d01b033a520408254bc58057b4ee798d1f
Anton <profox.rus@gmail.com>
2022-09-04 19:55:03 +0300

    Added advanced mode

--- lib/service_actor/collectionable.rb
+++ lib/service_actor/collectionable.rb
@@ -6,7 +6,17 @@
 # Example:
 #
 #   class Pay < Actor
-#     input :provider, in: ["MANGOPAY", "PayPal", "Stripe"]
+#     input :provider, inclusion: ["MANGOPAY", "PayPal", "Stripe"]
+#   end
+#
+#   class Pay < Actor
+#     input :provider,
+#           inclusion: {
+#             in: ["MANGOPAY", "PayPal", "Stripe"],
+#             message: (lambda do |_input_key, _in, value|
+#               "Payment system \"#{value}\" is not supported"
+#             end)
+#           }
 #   end
 module ServiceActor::Collectionable
   def self.included(base)
@@ -14,25 +24,27 @@ module ServiceActor::Collectionable
   end
 
   module PrependedMethods
-    def _call
+    def _call # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       self.class.inputs.each do |key, options|
-        next unless options[:in]
+        inclusion = options[:inclusion]
 
-        next if options[:in].include?(result[key])
+        if inclusion.is_a?(Hash) # advanced mode
+          inclusion_in, message = inclusion.values_at(:in, :message)
+          error_text = message.call(key, inclusion, result[key])
+        else
+          inclusion_in = inclusion
+          error_text = "Input #{key} must be included " \
+                       "in #{inclusion.inspect} but instead " \
+                       "was #{result[key].inspect}"
+        end
 
-        raise ArgumentError,
-              "Input #{key} must be included in #{options[:in].inspect} " \
-              "but instead was #{result[key].inspect}"
+        next if inclusion_in.nil?
+        next if inclusion_in.include?(result[key])
+
+        raise ArgumentError, error_text
       end
 
       super
     end
-
-    # private
-
-    # def error_text_with(key, options, result)
-    #   "Input #{key} must be included in #{options[:in].inspect} " \
-    #   "but instead was #{result[key].inspect}"
-    # end
   end
 end
