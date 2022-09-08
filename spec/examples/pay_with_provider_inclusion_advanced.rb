3c96db8c0c541585890a4b877543152e3c4e33ef
Anton <profox.rus@gmail.com>
2022-09-09 02:01:14 +0300

    Added example

---
+++ spec/examples/pay_with_provider_inclusion_advanced.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class PayWithProviderInclusionAdvanced < Actor
+  input :provider,
+        inclusion: {
+          in: %w[MANGOPAY PayPal Stripe],
+          message: (lambda do |_input_key, value, _inclusion_in|
+            "Payment system \"#{value}\" is not supported"
+          end)
+        },
+        default: "Stripe"
+
+  output :message, type: String
+
+  def call
+    self.message = "Money transferred to #{provider}!"
+  end
+end
