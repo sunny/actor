4c5c0086aca16635e3d8d82f7e2c40da015d19da
Anton <profox.rus@gmail.com>
2022-09-21 21:52:16 +0300

    Deleted `define_*` methods

--- lib/service_actor/raisable.rb
+++ lib/service_actor/raisable.rb
@@ -15,14 +15,6 @@ module ServiceActor::Raisable
       child.failure_class = failure_class || ServiceActor::Failure
     end
 
-    def define_argument_error_class(class_name)
-      self.argument_error_class = class_name
-    end
-
-    def define_failure_class(class_name)
-      self.failure_class = class_name
-    end
-
     attr_accessor :argument_error_class, :failure_class
   end
 end
