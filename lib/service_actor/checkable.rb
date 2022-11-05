071ebcdaeeb0896fb0d04a46eab57367c8a00989
Anton <profox.rus@gmail.com>
2022-11-06 01:55:53 +0300

    Refactoring

--- lib/service_actor/checkable.rb
+++ lib/service_actor/checkable.rb
@@ -7,15 +7,19 @@ module ServiceActor::Checkable
 
   module PrependedMethods
     def _call
-      inputs
+      checks_for(:input)
       super
-      outputs
+      checks_for(:output)
+
+      # inputs
+      # super
+      # outputs
     end
 
     private
 
-    def inputs # rubocop:disable Metrics/MethodLength
-      self.class.inputs.each do |input_key, input_options| # rubocop:disable Metrics/BlockLength
+    def checks_for(origin) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      self.class.send("#{origin}s").each do |input_key, input_options| # rubocop:disable Metrics/BlockLength
         input_options.each do |checker_name, checker_conditions| # rubocop:disable Metrics/BlockLength
           checkers = %w[
             TypeChecker
@@ -31,7 +35,7 @@ module ServiceActor::Checkable
 
             argument_errors = checker_class.for(
               checker_name: checker_name,
-              origin: :input,
+              origin: origin.to_sym,
               input_key: input_key,
               actor: self.class,
               value: result[input_key],
@@ -62,44 +66,92 @@ module ServiceActor::Checkable
       end
     end
 
-    def outputs # rubocop:disable Metrics/MethodLength
-      self.class.outputs.each do |input_key, input_options|
-        input_options.each do |checker_name, checker_conditions|
-          checkers = %w[
-            TypeChecker
-            NilChecker
-            DefaultChecker
-          ]
+    # def inputs
+    #   self.class.inputs.each do |input_key, input_options|
+    #     input_options.each do |checker_name, checker_conditions|
+    #       checkers = %w[
+    #         TypeChecker
+    #         MustChecker
+    #         InclusionChecker
+    #         NilChecker
+    #         DefaultChecker
+    #       ]
 
-          checkers.each do |checker_class_name|
-            checker_class =
-              Object.const_get("ServiceActor::Checkers::#{checker_class_name}")
+    #       checkers.each do |checker_class_name|
+    #         checker_class =
+    #           Object.const_get("ServiceActor::Checkers::#{checker_class_name}") # rubocop:disable Layout/LineLength
 
-            argument_errors = checker_class.for(
-              checker_name: checker_name,
-              origin: :output,
-              input_key: input_key,
-              actor: self.class,
-              value: result[input_key],
+    #         argument_errors = checker_class.for(
+    #           checker_name: checker_name,
+    #           origin: :input,
+    #           input_key: input_key,
+    #           actor: self.class,
+    #           value: result[input_key],
 
-              # TypeChecker
-              type_definition: checker_conditions,
-              given_type: result[input_key],
+    #           # TypeChecker
+    #           type_definition: checker_conditions,
+    #           given_type: result[input_key],
 
-              # NilChecker
-              allow_nil: checker_conditions,
+    #           # MustChecker
+    #           nested_checkers: checker_conditions,
 
-              # DefaultChecker
-              result: result,
+    #           # InclusionChecker
+    #           inclusion: checker_conditions,
 
-              # NilChecker + DefaultChecker
-              input_options: input_options,
-            )
+    #           # NilChecker
+    #           allow_nil: checker_conditions,
 
-            add_argument_errors(argument_errors)
-          end
-        end
-      end
-    end
+    #           # DefaultChecker
+    #           result: result,
+
+    #           # NilChecker + DefaultChecker
+    #           input_options: input_options,
+    #         )
+
+    #         add_argument_errors(argument_errors)
+    #       end
+    #     end
+    #   end
+    # end
+
+    # def outputs
+    #   self.class.outputs.each do |input_key, input_options|
+    #     input_options.each do |checker_name, checker_conditions|
+    #       checkers = %w[
+    #         TypeChecker
+    #         NilChecker
+    #         DefaultChecker
+    #       ]
+
+    #       checkers.each do |checker_class_name|
+    #         checker_class =
+    #           Object.const_get("ServiceActor::Checkers::#{checker_class_name}") # rubocop:disable Layout/LineLength
+
+    #         argument_errors = checker_class.for(
+    #           checker_name: checker_name,
+    #           origin: :output,
+    #           input_key: input_key,
+    #           actor: self.class,
+    #           value: result[input_key],
+
+    #           # TypeChecker
+    #           type_definition: checker_conditions,
+    #           given_type: result[input_key],
+
+    #           # NilChecker
+    #           allow_nil: checker_conditions,
+
+    #           # DefaultChecker
+    #           result: result,
+
+    #           # NilChecker + DefaultChecker
+    #           input_options: input_options,
+    #         )
+
+    #         add_argument_errors(argument_errors)
+    #       end
+    #     end
+    #   end
+    # end
   end
 end
