c0c8b721cc7549671a12857d7ce1427d245af9c5
viralpraxis <iaroslav2k@gmail.com>
2024-03-11 09:57:44 +0300

    Add simplecov as development dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter "/spec"
+end
+
 require "bundler/setup"
 require "service_actor"
 require "pry"
