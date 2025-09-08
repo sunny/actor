130438309cb10f6d350b76521aa7525d6b210d1d
Sunny Ripert <sunny@sunfox.org>
2025-09-08 10:41:27 +0200

    Add spec for stable instance methods

---
+++ spec/service_actor/core_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe ServiceActor::Core do
+  describe ".instance_methods" do
+    let(:expected_methods) { [:_call, :call, :fail!, :result, :rollback] }
+
+    it "stays small so as not to disallow too many possible input names" do
+      expect(described_class.instance_methods)
+        .to match_array(expected_methods)
+    end
+  end
+end
