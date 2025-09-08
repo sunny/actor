7d93b6e69a231094ead315e60e803e90d5ea423c
Sunny Ripert <sunny@sunfox.org>
2025-09-08 10:42:25 +0200

    Add comment

--- spec/service_actor/core_spec.rb
+++ spec/service_actor/core_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ServiceActor::Core do
   describe ".instance_methods" do
     let(:expected_methods) { [:_call, :call, :fail!, :result, :rollback] }
 
+    # See also https://github.com/sunny/actor/pull/207
     it "stays small so as not to disallow too many possible input names" do
       expect(described_class.instance_methods)
         .to match_array(expected_methods)
